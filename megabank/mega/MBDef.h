edf8c2d204d3c2849092b241f7a24b4acaa2b64b
aseevdanil <aseevdanil@gmail.com>
2016-12-25 21:51:25 +0300

    [megabank] Some fixes

--- megabank/mega/MBDef.h
+++ megabank/mega/MBDef.h
@@ -36,3 +36,34 @@ static NSString *const HEAD		= @"HEAD";
 static NSString *const DELETE	= @"DELETE";
 static NSString *const POST		= @"POST";
 static NSString *const PUT		= @"PUT";
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Singlton
+#define SINGLETON_DECL(singleton) + (instancetype) singleton;
+#define SINGLETON_IMPL(singleton)		\
++ (instancetype) singleton			\
+{									\
+static id Singletone = nil;	\
+static dispatch_once_t once;	\
+dispatch_once(&once, ^{ Singletone = [[self alloc] init]; });	\
+return Singletone;				\
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Safe invocation
+#define SAFE_CALL_WEAK_TARGET_METHOD(__weakMethodTarget, __methodSelector) \
+{ \
+id __strongMethodTarget = __weakMethodTarget; \
+if (__strongMethodTarget && __methodSelector) \
+((void (*)(id, SEL))[__strongMethodTarget methodForSelector:__methodSelector])(__strongMethodTarget, __methodSelector); \
+}
+
+#define SAFE_CALL_WEAK_TARGET_METHOD_WITH_OBJECT(__weakMethodTarget, __methodSelector, __methodObject) \
+{ \
+id __strongMethodTarget = __weakMethodTarget; \
+if (__strongMethodTarget && __methodSelector) \
+((void (*)(id, SEL, id))[__strongMethodTarget methodForSelector:__methodSelector])(__strongMethodTarget, __methodSelector, __methodObject); \
+}
